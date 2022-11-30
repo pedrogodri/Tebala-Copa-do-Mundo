4b83f25fb7446964a49aba592f78ec454316f577
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-11-29 22:33:08 -0300

    Commit - Adicionando a tabela dos jogos

---
+++ _js/main.js
@@ -0,0 +1,19 @@
+let tabelaJogos = document.querySelector('.tabelaJogos');
+
+fetch('jogos-fase1.json')
+    .then(response => response.json())
+    .then(data => data.forEach(jogo => {
+
+        // Criar uma linha de tabela, coloacar ela na tabela
+        let linha = document.createElement('tr');
+        tabelaJogos.appendChild(linha);
+        // Preencher os dados do jogo em cada linnha da tabela
+        linha.innerHTML = `
+            <td>${jogo.diaSemana}</td>
+            <td>${jogo.data}</td>
+            <td>${jogo.hora}</td>
+            <td>${jogo.grupo}</td>
+            <td>${jogo.partida}</td>
+            <td>${jogo.estadio}</td>
+        `
+    }));
