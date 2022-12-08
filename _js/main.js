a4835fbad96ed41f03a35fdbb81fbc6775c8cfa8
Pedro Henrique Godri <pedro.g2006@aluno.ifsc.edu.br>
2022-12-08 13:07:15 -0300

    Commit - Adicionando as bandeiras das seleções

--- _js/main.js
+++ _js/main.js
@@ -13,7 +13,15 @@ fetch('jogos-fase1.json')
             <td>${jogo.data}</td>
             <td>${jogo.hora}</td>
             <td>${jogo.grupo}</td>
-            <td>${jogo.partida}</td>
+            <td class='centralizar'>
+                <img class='imagemP' src='./_img/bandeiras/${jogo.mandante}' alt='Escudo Seleção Mandante'/>
+                
+                <span class='gols'>${jogo.gols_mandante}</span>
+                <span class='partida'>${jogo.partida}</span>
+                <span class='gols'>${jogo.gols_visitante}</span>
+
+                <img class='imagemP' src='./_img/bandeiras/${jogo.visitante}' alt='Escudo Seleção Visitante'/>
+            </td>
             <td id='estadio'>${jogo.estadio}</td>
         `
     }));
